648357685fd0746cd7388aa1832a1e1f4e5057c8
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-11-16 11:23:21 +0300

    reconfiguring deployment

--- config/environment.rb
+++ config/environment.rb
@@ -5,16 +5,16 @@ Bundler.require(:default, ENV['RACK_ENV'])
 
 require_all 'app'
 
-configure :production, :development do
-    db = URI.parse(ENV['RACK_ENV'] ||
-    'sqlite3://localhost/db/development.sqlite3'
-    )
-    ActiveRecord::Base.establish_connection(
-        adapter: 'sqlite3',
-        host: db.host,
-        username: db.user,
-        password: db.password,
-        database: db.path[1..-1],
-        encoding: 'utf8'
-    )
-end
+# configure :production, :development do
+#     db = URI.parse(ENV['RACK_ENV'] ||
+#     'sqlite3://localhost/db/development.sqlite3'
+#     )
+#     ActiveRecord::Base.establish_connection(
+#         adapter: 'sqlite3',
+#         host: db.host,
+#         username: db.user,
+#         password: db.password,
+#         database: db.path[1..-1],
+#         encoding: 'utf8'
+#     )
+# end
